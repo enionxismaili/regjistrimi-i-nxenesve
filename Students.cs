0ce13c21a3f9a700bdce62e400c33841f4db20e5
enionxismaili <enion.ismaili@gmail.com>
2024-02-06 08:49:42 +0100

    Add files via upload

---
+++ Students.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorServerTest.Models
+{
+    public class Students
+    {
+/*        public int id { get; set; }
+        public string emri { get; set; }
+        public string mbiemri { get; set; }
+        public string vendlindja {  get; set; }
+        public string vendbanimi { get; set; }
+        public string emaili { get; set; }
+        public double notamesatare {  get; set; }
+        public double piket {  get; set; }
+        public int notasjelljes { get;set; }
+        public string numritel { get; set; }
+        public string email {  get; set; }
+*/
+
+
+    }
+}
